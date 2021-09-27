a523c65f592590ed80f8aaa2aac0ac7e3ef42a22
Martin <martin.stankard@hotmail.com>
2021-09-26 21:27:14 -0700

    02 var done

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -2,8 +2,29 @@ package main
 
 import "fmt"
 
-func main(){
-	var str string = "Hello World"
+//var str  = "Hello World"
 
-	fmt.Println(str)
+
+func main(){
+	///BEHOLD THE POWER OF INFERRED TYPES!!!!
+	//var str string = "Hello World"
+	//var str  = "Hello World"
+	str := ":= is the phalic method of variable declaration!!"+
+	"Can only be used within a function"
+
+
+	//var age int = 39
+	var age uint64 = 39
+	//uint8 is the weirdest one and it works.
+
+	const isCool = true
+
+	// this is a float64 size := 1.333
+	const size float32 = 1.337
+
+	fmt.Println(str, age, isCool, size)
+	fmt.Printf("\"%s\" is a %T\n", str, str)
+	fmt.Printf("%d is a %T\n", age, age)
+	fmt.Printf("%t is a %T\n", isCool, isCool)
+	fmt.Printf("%f is a %T\n", size, size)
 }
