b0c50225abfd2fb723d02c7aa738baaec3103a9d
Martin <martin.stankard@hotmail.com>
2021-10-02 05:26:09 -0700

    change gears

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World")
+func greeting(name string) string {
+	return "Hello " + name
+}
+
+func sum(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func main() {
+	fmt.Println(greeting("Martin"))
+	fmt.Println(sum(2,3))
 }
