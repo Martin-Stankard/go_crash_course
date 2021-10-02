4a27f2780da47d580475e351b6edf81e5020913c
Martin <martin.stankard@hotmail.com>
2021-10-02 12:52:15 -0700

    onto closures

--- 10_pointers/main.go
+++ 10_pointers/main.go
@@ -5,5 +5,17 @@ import "fmt"
 
 func main() {
 	fmt.Println("pointers")
+	a := 5
+	//annoying like C++, space optional
+	pointerOfA := &  a
+	valueOfPointerOfA := *pointerOfA
+
+	fmt.Println(a)
+	fmt.Println(pointerOfA)
+	fmt.Println(valueOfPointerOfA)
+
+	*pointerOfA = 10
+	fmt.Println(a)
+	
 }
 
